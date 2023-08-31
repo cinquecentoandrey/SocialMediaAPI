67f6fcdc8fd039ee8ec693e0f102662b4a006386
Andrey <chehonte16@gmail.com>
2023-08-31 20:26:20 +0300

    Added user details service

---
+++ src/main/java/com/cinquecento/smapi/service/UserDetailsService.java
@@ -0,0 +1,32 @@
+package com.cinquecento.smapi.service;
+
+import com.cinquecento.smapi.model.User;
+import com.cinquecento.smapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User with username: " + username + " not found");
+        }
+
+        return new com.cinquecento.smapi.security.UserDetails(user.get());
+    }
+}

ce3de3ab662de4ea1c18f9bbc50be2600a8354b1
Andrey <chehonte16@gmail.com>
2023-09-01 22:13:05 +0300

    Marked @Repository

--- src/main/java/com/cinquecento/smapi/repository/PostRepository.java
+++ src/main/java/com/cinquecento/smapi/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.cinquecento.smapi.repository;
 
 import com.cinquecento.smapi.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }

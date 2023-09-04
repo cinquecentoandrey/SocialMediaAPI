c324c8ed2b2ed79ef51e510f1908a5598fd57ef6
Andrey <chehonte16@gmail.com>
2023-09-04 22:58:18 +0300

    Added custom queries in repositories

--- src/main/java/com/cinquecento/smapi/repository/PostRepository.java
+++ src/main/java/com/cinquecento/smapi/repository/PostRepository.java
@@ -1,9 +1,33 @@
 package com.cinquecento.smapi.repository;
 
 import com.cinquecento.smapi.model.Post;
+import com.cinquecento.smapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByCreator_Id(Long id);
+
+    @Modifying
+    @Query("UPDATE Post p SET p.article=:article, p.content=:content WHERE p.id=:id")
+    void partUpdate(@Param("id") Long id,
+                    @Param("article") String article,
+                    @Param("content") String content);
+
+    Optional<Post> findByCreator(User creator);
+
+    @Query(value = "SELECT DISTINCT P.*\n" +
+            "FROM Friends AS F\n" +
+            "JOIN Users AS U ON (F.first_user_id = U.id OR F.second_user_id = U.id) and (U.id <> :userId) \n" +
+            "Join Posts AS P ON p.user_id = u.id\n" +
+            "WHERE F.first_user_id= :userId OR F.second_user_id= :userId", nativeQuery = true)
+    List<Post> findAllFriendsPosts(@Param("userId") Long userId);
 }
--- src/main/java/com/cinquecento/smapi/repository/UserRepository.java
+++ src/main/java/com/cinquecento/smapi/repository/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -28,4 +29,32 @@ public interface UserRepository extends JpaRepository<User, Long> {
                     @Param(value = "email") String email,
                     @Param(value = "last_update") Date last_update);
 
+    @Query(value = "SELECT DISTINCT " +
+            "U.*\n" +
+            "FROM Friends AS F\n" +
+            "JOIN Users AS U ON (F.fist_user_id = U.id OR F.second_user_id = U.id) and (U.id <> :id)\n" +
+            "WHERE F.fist_user_id= :id OR F.second_user_id= :id", nativeQuery = true)
+    List<User> findFriendsByUserId(@Param("id") Long id);
+
+    @Query(value = "SELECT DISTINCT U.*\n" +
+            "FROM Followers AS F\n" +
+            "JOIN Users AS U ON (F.user_id = U.id OR F.follower_id = U.id) and (U.id <> :id) \n" +
+            "WHERE F.user_id= :id OR F.follower_id= :id", nativeQuery = true)
+    List<User> findFollowersByUserId(@Param("id") Long id);
+
+    @Modifying
+    @Query(value = "INSERT INTO Followers values(:userId, :subId)", nativeQuery = true)
+    void subscribe(@Param("userId") Long userId, @Param("subId") Long subId);
+
+    @Modifying
+    @Query(value = "DELETE FROM Followers WHERE user_id=:userId AND follower_id=:unsubId", nativeQuery = true)
+    void unsubscribe(@Param("userId") Long userId, @Param("unsubId") Long unsubId);
+
+    @Modifying
+    @Query(value = "INSERT INTO Friends values(:userId, :subId)", nativeQuery = true)
+    void addFriend(@Param("userId") Long id, @Param("subId") Long friendId);
+
+    @Modifying
+    @Query(value = "DELETE FROM Friends WHERE first_user_id=:firstId AND second_user_id=:secondId", nativeQuery = true)
+    void removeFriend(@Param("firstId") Long firstId, @Param("secondId") Long secondId);
 }

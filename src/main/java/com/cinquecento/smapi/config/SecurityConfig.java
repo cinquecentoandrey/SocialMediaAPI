84e2fcfb861bfe96fdaae19e7c63b60948ae0a01
Andrey <chehonte16@gmail.com>
2023-09-04 23:10:18 +0300

    Added role rule in SecurityConfig

--- src/main/java/com/cinquecento/smapi/config/SecurityConfig.java
+++ src/main/java/com/cinquecento/smapi/config/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     private final JWTFilter jwtFilter;
@@ -32,6 +31,7 @@ public class SecurityConfig {
                 .csrf().disable()
                 .authorizeRequests()
                 .requestMatchers("/api/v1/auth/**" ).permitAll()
+                .anyRequest().hasAnyRole("USER", "ADMIN")
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);

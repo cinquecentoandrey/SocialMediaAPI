aaba46f68fd57d086c6662cfe2cde8e71bdf9412
Andrey <chehonte16@gmail.com>
2023-09-08 10:55:35 +0300

    Added methods for sorting pots

--- src/main/java/com/cinquecento/smapi/repository/PostRepository.java
+++ src/main/java/com/cinquecento/smapi/repository/PostRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,18 +17,24 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findAllByCreator_Id(Long id);
 
+    List<Post> findAllByCreator_IdOrderByLastUpdateDesc(Long id);
+
+    List<Post> findAllByCreator_IdOrderByLastUpdateAsc(Long id);
+
     @Modifying
-    @Query("UPDATE Post p SET p.article=:article, p.content=:content WHERE p.id=:id")
+    @Query("UPDATE Post p SET p.article=:article, p.content=:content, p.lastUpdate=:lastUpdate WHERE p.id=:id")
     void partUpdate(@Param("id") Long id,
                     @Param("article") String article,
-                    @Param("content") String content);
+                    @Param("content") String content,
+                    @Param("lastUpdate") Date lastUpdate);
 
-    Optional<Post> findByCreator(User creator);
+    Optional<Post> findByCreatorAndId(User creator, Long postId);
 
     @Query(value = "SELECT DISTINCT P.*\n" +
             "FROM Friends AS F\n" +
             "JOIN Users AS U ON (F.first_user_id = U.id OR F.second_user_id = U.id) and (U.id <> :userId) \n" +
             "Join Posts AS P ON p.user_id = u.id\n" +
-            "WHERE F.first_user_id= :userId OR F.second_user_id= :userId", nativeQuery = true)
+            "WHERE F.first_user_id=:userId OR F.second_user_id=:userId", nativeQuery = true)
     List<Post> findAllFriendsPosts(@Param("userId") Long userId);
+
 }
